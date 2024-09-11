9f1f27699f6d3d3247c9661e7453bcc0f9072257
rafansyads <rafansya2005@gmail.com>
2024-09-11 14:05:57 +0700

    1.0.0: Finishing Tutorial 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rafansya-daryltama-mentalhealthtracker.pbp.cs.ui.ac.id"]
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost",
+                        "http://127.0.0.1",
+                        "http://rafansya-daryltama-mentalhealthtracker.pbp.cs.ui.ac.id", 
+                        "https://rafansya-daryltama-mentalhealthtracker.pbp.cs.ui.ac.id"
+                        ]
