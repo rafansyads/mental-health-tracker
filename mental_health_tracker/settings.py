7e9b81fdc9deccbe35150295f7e40b7fe875be40
rafansyads <rafansya2005@gmail.com>
2024-09-25 13:20:38 +0700

    Revert "0.2.3: Finishing Tutorial 3"
    This reverts commit a83156e255a9c6b2f03d5a2b37963c019244fcbf.

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&_pm&tjop=difk+eth74wb&)or)c*e9uox*44#r)1#*r-$!)b7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-PRODUCTION = os.getenv("PRODUCTION", False)
-DEBUG = not PRODUCTION
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
