ea3bea2ccb7570f7a306e5cc34acc31b0df36267
rafansyads <rafansya2005@gmail.com>
2024-10-02 20:36:11 +0700

    Revert "0.4.0: Finishing Tutorial 5"
    This reverts commit b491f811e7878e6d63c57e670a69da756d0ba132.

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from main.views import show_main
-from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id,\
-        show_json_by_id, edit_mood, delete_mood, register, login_user, logout_user, add_mood_entry_ajax
+from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, edit_mood, delete_mood, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -16,6 +15,5 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('edit-mood/<uuid:id>/', edit_mood, name='edit_mood'),
-    path('delete-mood/<uuid:id>/', delete_mood, name='delete_mood'),
-    path('create-mood-entry-ajax/', add_mood_entry_ajax, name='add_mood_entry_ajax'),
+    path('delete-mood/<uuid:id>/', delete_mood, name='delete_mood')
 ]
